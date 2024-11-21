13993917462950994a0290ed4502bd7843f32c2e
Irene Musau <irenemwikalii04@gmail.com>
2024-11-21 08:07:54 +0300

    done

--- server/werkzeug_app.py
+++ server/werkzeug_app.py
@@ -11,6 +11,6 @@ if __name__ == '__main__':
     from werkzeug.serving import run_simple
     run_simple(
         hostname='localhost',
-        port=5555,
+        port=5000,
         application=application
     )
